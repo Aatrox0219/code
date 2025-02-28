ecf9b6e2cc44aa5209c13eb9df20fd22ed951511
Villanelle <m18827088079@163.com>
2025-02-28 17:05:22 +0800

    流程id修改

--- src/api/processId.js
+++ src/api/processId.js
@@ -1,11 +1,15 @@
 //保证金存缴的流程id列表和分类id
-export const depositList = ['5', '7', '9', '11'];
+export const depositList = ['5210', '5212', '5214', '5216'];
 export const depositCategoryId = '1847453055727501313';
 
 //保证金使用的流程id列表和分类id
-export const useList = ['5', '7', '9', '11', '345'];
+export const useList = ['5210', '5212', '5214', '5216', '5218'];
 export const useCategoryId = '1847453556447707137';
 
 //保证金补缴的流程id列表和分类id
-export const backpayList = ['5', '7', '9', '11', '345', '40060', '40062', '40064', '40066'];
+export const backpayList = ['5210', '5212', '5214', '5216', '5218', '5220', '5222', '5224', '5226'];
 export const backpayCategoryId = '1860939985686949889';
+
+//保证金存缴方式变更的流程id列表和分类id
+export const changeList = ['5210', '5212', '5214', '5216', '5228', '5230', '5232', '5234'];
+export const changeCategoryId = '1860602147955077121';
